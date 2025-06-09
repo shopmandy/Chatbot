c2b3e7bdbe30f7e6a4d6cf49172dc16533731d0f
henryshan30 <henry_shan@brown.edu>
2025-06-09 15:16:28 -0400

    add shop to navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,24 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <>
-    <nav style={{ padding: '1rem', backgroundColor: '#f0f0f0' }}>
-      <Link className="nav-link" href="/" style={{ marginRight: '1rem' }}>HOME</Link>
-      <Link className="nav-link" href="/about" style={{ marginRight: '1rem' }}>ABOUT</Link>
-      <Link className="nav-link" href="/chatbot">CHATBOT</Link>
-    </nav>
-    <main style={{ padding: '1rem' }}>
-      <Component {...pageProps} />
-    </main>
-  </>);
+    <>
+      <nav style={{ padding: '1rem', backgroundColor: '#f0f0f0' }}>
+        <Link className="nav-link" href="/" style={{ marginRight: '1rem' }}>HOME</Link>
+        <Link className="nav-link" href="/about" style={{ marginRight: '1rem' }}>ABOUT</Link>
+        <Link className="nav-link" href="/chatbot" style={{ marginRight: '1rem' }}>CHATBOT</Link>
+        <a 
+          className="nav-link" 
+          href="https://shopmandy.com/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          style={{ marginRight: '1rem' }}
+        >
+          SHOP
+        </a>
+      </nav>
+      <main style={{ padding: '1rem' }}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
